48926389f9f5687cd7cb47d2d412811707d04558
Khayyam Saleem <khayyam.saleem@gmail.com>
2016-06-08 18:24:06 -0400

    Update output.js

--- js/output.js
+++ js/output.js
@@ -4,11 +4,11 @@ var view = {
         var today = new Date();
         return today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
         },
-    name: "Khayyam Saleem",
-    occupation: "developer"
+    name: "Khayyam Saleem educating in fartfart 101 ",
+    occupation: "professional tooter"
 };
 
-var out = "This is my {{title}}. Today is {{date}}. My name is {{name}}. I am a {{occupation}}.";
+var out = "This is my {{title}}. Today is {{date}}. My name is farty {{name}}. I am a {{occupation}} actually i am a pro farter.";
 
 var output = Mustache.render(out, view);
 
