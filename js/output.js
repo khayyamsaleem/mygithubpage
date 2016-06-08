eee6bb9ba150283842a8db6aeb328eb965f2419e
Khayyam Saleem <khayyam.saleem@gmail.com>
2016-06-08 18:42:23 -0400

    fixed vishali's nonsense

--- js/output.js
+++ js/output.js
@@ -4,11 +4,11 @@ var view = {
         var today = new Date();
         return today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
         },
-    name: "Khayyam Saleem educating in fartfart 101 ",
-    occupation: "professional tooter"
+    name: "Khayyam Saleem",
+    occupation: "developer"
 };
 
-var out = "This is my {{title}}. Today is {{date}}. My name is farty {{name}}. I am a {{occupation}} actually i am a pro farter.";
+var out = "This is my {{title}}. Today is {{date}}. My name is {{name}}. I am a {{occupation}}.";
 
 var output = Mustache.render(out, view);
 
