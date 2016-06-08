71da5da25df7244b6bd2f2a6aa4ea4c5b594245d
ksaleem <ksaleem@iconectiv.com>
2016-06-08 19:32:04 -0400

    trying to set attributes for DOM elements using javascript

--- js/output.js
+++ js/output.js
@@ -14,6 +14,9 @@ var output = Mustache.render(out, view);
 
 document.getElementById("output").innerHTML = output;
 
+var name = document.createElement("DIV");
+document.body.appendChild(name);
+document.body.div.setAttribute("id", "output");
 var style = document.createElement("STYLE");
 var outputStyle = document.createTextNode('#output {text-align: center; font-family: "Roboto", sans-serif; font-weight: normal; font-size: 1em; color: #000;}');
 style.appendChild(outputStyle);
