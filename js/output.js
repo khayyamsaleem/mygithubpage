86ec6d659960329114e6dc1602a2d0a3a409021d
ksaleem <ksaleem@iconectiv.com>
2016-06-08 19:23:59 -0400

    trying styling through javascript

--- js/output.js
+++ js/output.js
@@ -8,10 +8,13 @@ var view = {
     occupation: "developer"
 };
 
-var out = "This is my {{title}}. Today is {{date}}. My name is {{name}}. I am a {{occupation}}.";
+var out = "This is my {{title}}.<br> Today is {{date}}.<br> My name is {{name}}.<br> I am a {{occupation}}.";
 
 var output = Mustache.render(out, view);
 
 document.getElementById("output").innerHTML = output;
-document.getElementById("output").style.color = "red"; 
-document.getElementById("output").style.textAlign = "center";
+
+var style = document.createElement("STYLE");
+var outputStyle = document.createTextNode('#output {text-align: center; font-family: "Roboto", sans-serif; font-weight: normal; font-size: 1em; color: #000;}');
+style.appendChild(outputStyle);
+document.head.appendChild(style);
