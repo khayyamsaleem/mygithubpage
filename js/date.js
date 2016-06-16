a123703ccf06acd4db1eab2b54449f0fbfa375c2
Khayyam Saleem <khayyamsaleem@Khayyams-MacBook-Pro.local>
2016-06-16 16:41:16 -0400

    updated motivation

--- js/date.js
+++ js/date.js
@@ -1,10 +1,10 @@
 var person = {
-    first: '',
-    last: '',
-    dob: ''
+    first: 'Khayyam',
+    last: 'Saleem',
+    dob: '12/12/1997'
 };
 
-var template = "<h1>{{first}} {{last}}</h1>";
+var template = "<h1>{{first}} {{last}}</h1><br><br><p>{{dob}}</p>";
 
 var html = Mustache.to_html(template, person);
 $('#test').html(html);
