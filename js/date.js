3e696bd0354381aad968d60d0977199f680f1b28
Khayyam Saleem <khayyamsaleem@Khayyams-MacBook-Pro.local>
2016-06-16 17:15:42 -0400

    learning jquery

--- js/date.js
+++ js/date.js
@@ -9,3 +9,18 @@ var template = "<h1>{{first}} {{last}}</h1><br><br><p>{{dob}}</p>";
 var html = Mustache.to_html(template, person);
 $('#test').html(html);
 
+$( "form" ).submit(function( event ) {
+    if ( $( "input:#m" ).val() === "12" && 
+         $( "input:#d" ).val() === "12" &&
+         $( "input:#y" ).val() === "1997" ) {
+         $ ( "bday" ).text("That is your birthday!").show();
+         return;
+    }
+
+    $( "bday" ).text( "Nah" ).show().fadeOut( 1000 );
+    event.preventDefault();
+});
+
+
+
+
