0c4fc1ef52904106d853b0868bd4b77eda2b17de
Khayyam Saleem <khayyam.saleem@gmail.com>
2016-06-27 01:26:37 -0400

    cross browser ish

--- motivation/date.js
+++ motivation/date.js
@@ -9,6 +9,17 @@ var ageCalc = function() {
 };
 
 $(document).ready(function(){
+	if ((navigator.userAgent.toLowercase().indexOf('webkit') > 0) && (typeOf localStorage['dob'] == 'undefined')){
+		$('#pikaday').remove();
+		$('footer').html("<input type='date' id='datepicker' name='age' /> <button id='button'>Motivate</button>");
+		$('#button').click(function(e){
+			e.preventDefault();
+			localStorage['dob'] = new Date($('#datepicker').value);
+			$('form').fadeOut('fast');
+			$('body').append("div id='age'></div>");
+			setInterval(ageCalc, 80);
+		};
+	};
 	if (typeof localStorage['dob'] != "undefined"){
 		$('form').remove();
 		$('body').append("<div id='age'></div>");
