1f1301ef666bde8b219627a00299303017827d20
Khayyam Saleem <khayyamsaleemgmail.com@>
2016-12-16 14:12:15 -0500

    tmp for cc

---
+++ cc/test.js
@@ -0,0 +1,29 @@
+var Chav = {    
+    firstName : "Chav",        
+    lastName : "Whatever",
+    description : "Chav works at ZEFR! He leads our engineering team in building ZEFR software.",    
+    selector : ".add-Chav"
+}
+
+var Charlie = {
+    firstName : "Charlie",   
+    lastName : "Jones",    
+    description : "Chav works at ZEFR! He is a claimer on our RightsID publishing team",  
+    selector : ".add-Charlie"
+}
+
+var Jason = {
+    firstName : "Jason",
+    lastName : "Nothing",
+    description: "Jason is on our Influencer team. He is helps put together projects for our clients on YouTube.",
+    selector : ".add-Jason"
+ }
+
+var team = [Chav, Charlie, Jason];
+
+for (var i in team) {
+    $(team[i].selector).on("click", function() {
+        console.log("clicked");
+        //this.fadeOut("slow");
+    });
+}
